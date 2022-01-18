a83d9af18ec27c43dace39fa67730b825e9f929c
Pijus9606 <pijusnari@gmail.com>
2022-01-18 15:21:07 +0200

    Create Space post list

--- src/Nasa.js
+++ src/Nasa.js
@@ -5,27 +5,38 @@ const URL = "https://api.nasa.gov/planetary/apod";
 
 const API_KEY = "?api_key=tHiwRhBbKEcrarVQgMikSYVugkHyygZ3ydzQaekZ";
 
+const DATE = "&start_date="
+const DATETWO = "&end_date=";
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var yesterday = new Date(new Date().setDate(new Date().getDate()-10));
+var yesterdayDate = yesterday.getFullYear()+'-'+(yesterday.getMonth()+1)+'-'+yesterday.getDate();
+
+
+const startDay = yesterdayDate;
+const endDay = date;
+
+
 export default class Clock extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: "",
-      img: "",
-      date: "",
-      explanation: "",
-      isloading: true
+      isloading: true,
+      spacePosts: []
     };
   }
 
   componentDidMount() {
-    fetch(URL + API_KEY)
+    fetch(URL + API_KEY + DATE + startDay + DATETWO + endDay)
     .then(response => response.json())
     .then(data => {
-      this.setState({title: data.title, img: data.url, date: data.date, explanation: data.explanation, isloading: false})
+      this.setState({isloading: false, spacePosts: data})
     });
   }
 
   render() {
+    console.log(this.state)
     if (this.state.isloading) {
       return <div>loading</div>
     }
@@ -33,7 +44,9 @@ export default class Clock extends React.Component {
     return (
       <div>
         <h1>Hello, world! This is Spacestagram.</h1>
-       <SpacePost title={this.state.title} date={this.state.date} img={this.state.img} explanation={this.state.explanation}/>
+       {this.state.spacePosts.map((post) => {
+         return <SpacePost title={post.title} date={post.date} img={post.url} explanation={post.explanation}/>
+       })}
       </div>
     )
   }
